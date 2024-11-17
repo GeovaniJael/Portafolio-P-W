1e1edbbf06afa5ba71f1699e70c720710ba80c4a
GeovaniJael <22690324@tecvalles.mx>
2024-11-17 10:15:27 -0600

    Add files via upload

---
+++ js/pokeapi.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("search-form");
+const nombreTxt = document.getElementById("pokemon-name");
+const typesList = document.getElementById("pokemon-types");
+const image = document.getElementById("pokemon-image");
+function clearResults() {
+    nombreTxt.innerText = "";
+    typesList.innerHTML = "";
+    image.setAttribute("src", "");
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const pokemonName = document.getElementById("pokemon-name-input").value.toLowerCase();
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        .then((response) => {
+            if (!response.ok) throw new Error("Pokémon no encontrado");
+            return response.json();
+        })
+        .then((pokemon) => {
+            clearResults();
+            nombreTxt.innerText = pokemon.name;
+
+            const lista = document.createElement("ul");
+            pokemon.types.forEach((tipo) => {
+                const item = document.createElement("li");
+                item.innerText = tipo.type.name;
+                item.classList.add(`type-${tipo.type.name}`);
+                lista.appendChild(item);
+            });
+            typesList.appendChild(lista);
+
+            image.setAttribute("src", pokemon.sprites.front_shiny);
+        })
+        .catch((error) => {
+            clearResults();
+            nombreTxt.innerText = "Error: " + error.message;
+            console.error(error);
+        });
+});
